97cfa54e4056f73f495bf04ef6e548e1e8f3a68b
Liksu <github@Liksu.kiev.ua>
2012-11-29 02:59:34 +0200

    Iterative update

--- content/room/room.js
+++ content/room/room.js
@@ -14,12 +14,26 @@ function(wo) {
 
 	self.ready = function() {
 		self.$rendered.addClass('window');
+		self.$rendered.draggable({handle: self.events.sl['caption']});
+		self.$rendered.resizable();
 	};
 
 	self.events.set('click.window', function(e) {
-		ok('room');
-		self.$rendered.toggleClass('active');
+		self.$rendered.addClass('active');
 		e.stopPropagation();
 		e.preventDefault();
 	});
+
+	self.events.set('click.minimize', function(e) {
+		$(e.target)
+			.toggleClass('minimize')
+			.toggleClass('maximize')
+		;
+		self.$rendered.toggleClass('collapsed');
+		self.$('.ui-resizable-handle').toggle();
+	});
+
+	setInterval(function() {
+		self.$('unread').toggleClass('blink');
+	}, 800);
 }
